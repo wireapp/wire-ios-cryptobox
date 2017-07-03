5a8c5c7879c984dd6fad305392cca601483d9968
Marco Conti <marcoconti83@gmail.com>
2017-07-03 11:41:59 +0200

    Add xcconfig for macOS

---
+++ WireCryptobox-macOS/WireCryptobox-macOS.h
@@ -0,0 +1,19 @@
+//
+//  WireCryptobox-macOS.h
+//  WireCryptobox-macOS
+//
+//  Created by Marco Conti on 22.05.17.
+//  Copyright © 2017 Cryptobox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WireCryptobox-macOS.
+FOUNDATION_EXPORT double WireCryptobox_macOSVersionNumber;
+
+//! Project version string for WireCryptobox-macOS.
+FOUNDATION_EXPORT const unsigned char WireCryptobox_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WireCryptobox_macOS/PublicHeader.h>
+
+
