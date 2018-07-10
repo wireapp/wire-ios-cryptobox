130aec1a56cf5e467bf4b6e81b6c97f9bcf65534
Gerasimenko Mike <mike@gerasimenko.me>
2018-07-10 16:35:33 +0200

    Reimplemented caching

--- WireCryptobox/EncryptionSessionsDirectoryCaching.swift
+++ WireCryptobox/EncryptionSessionsDirectoryCaching.swift
@@ -20,34 +20,130 @@ import Foundation
 
 // Caching proxy for @c backingEncryptor
 class CachingEncryptor: Encryptor {
+    
+    class Cache<Key: Hashable, Value> {
+        private var cache: [Key: EntryMetadata<Value>] = [:]
+        private let maxCost: Int
+        private let maxElements: Int
+        
+        private struct EntryMetadata<Value> {
+            let value: Value
+            let cost: Int
+            let insertedAt: TimeInterval
+            
+            init(value: Value, cost: Int) {
+                self.value = value
+                self.cost = cost
+                self.insertedAt = Date().timeIntervalSinceReferenceDate
+            }
+        }
+        
+        init(maxCost: Int, maxElements: Int) {
+            assert(maxCost > 0, "maxCost must be greather than 0")
+            assert(maxElements > 0, "maxElements must be greather than 0")
+            self.maxCost = maxCost
+            self.maxElements = maxElements
+        }
+        
+        func set(value: Value, for key: Key, cost: Int) {
+            assert(cost > 0, "Cost must be greather than 0")
+            cache[key] = EntryMetadata(value: value, cost: cost)
+            purgeCacheIfNeeded()
+        }
+        
+        func value(for key: Key) -> Value? {
+            return cache[key]?.value
+        }
+        
+        private func purgeCacheIfNeeded() {
+            purgeBasedOnElementsCount()
+            purgeBasedOnCost()
+        }
+        
+        private func sorted() -> [(Key, EntryMetadata<Value>)] {
+            return cache.map { key, metadata in (key, metadata) }.sorted { left, right in
+                left.1.insertedAt < right.1.insertedAt
+            }
+        }
+        
+        private func currentCost() -> Int {
+            return cache.reduce(0) { result, element in
+                return result + element.value.cost
+            }
+        }
+        
+        private func purgeBasedOnElementsCount() {
+            guard cache.count > maxElements else {
+                return
+            }
+            
+            let sorted = self.sorted()
+            
+            sorted.prefix(upTo: cache.count - maxElements).forEach { (key, _) in
+                cache[key] = nil
+            }
+        }
+        
+        private func purgeBasedOnCost() {
+            let currentCost = self.currentCost()
+            
+            guard currentCost > maxCost else {
+                return
+            }
+            
+            let costToClear = currentCost - maxCost
+            
+            let sorted = self.sorted()
+            
+            var accumulatedCost: Int = 0
+            var index: Int = 0
+            
+            for (_, metadata) in sorted {
+                index = index + 1
+                accumulatedCost = accumulatedCost + metadata.cost
+                
+                if accumulatedCost >= costToClear {
+                    break
+                }
+            }
+        
+            sorted.prefix(upTo: index).forEach { (key, _) in
+                cache[key] = nil
+            }
+        }
+    }
+    
     private unowned let backingEncryptor: Encryptor
-    private let cache = NSCache<NSString, NSData>()
+    // The maximum size of the end-to-end encrypted payload is defined by ZMClientMessageByteSizeExternalThreshold
+    // It's currently 128KB of data. We will allow up to 8 messages of maximum size to persist in the cache.
+    private let cache = Cache<GenericHash, Data>(maxCost: 1_000_000, maxElements: 100)
     
     deinit {
         zmLog.debug("Cache flushed / deallocated")
     }
     
-    public init(encryptor: Encryptor) {
+    init(encryptor: Encryptor) {
         backingEncryptor = encryptor
-        
-        cache.countLimit = 100
-        cache.name = "Encrypted payloads cache"
-        // The maximum size of the end-to-end encrypted payload is defined by ZMClientMessageByteSizeExternalThreshold
-        // It's currently 128KB of data. We will allow up to 8 messages of maximum size to persist in the cache.
-        cache.totalCostLimit = 1_000_000 // = Max 1 MB of data
     }
     
-    public func encrypt(_ plainText: Data, for recipientIdentifier: WireCryptobox.EncryptionSessionIdentifier) throws -> Data {
-        let cacheID: NSString = ("\(plainText.hashValue)" + recipientIdentifier.rawValue) as NSString
-        
-        if let cachedObject = cache.object(forKey: cacheID) {
-            zmLog.debug("Cache hit: \(cacheID)")
-            return cachedObject as Data
+    private func hash(for data: Data, recepient: EncryptionSessionIdentifier) -> GenericHash {
+        let builder = GenericHashBuilder()
+        builder.append(data)
+        builder.append(recepient.rawValue.data(using: .utf8)!)
+        return builder.build()
+    }
+    
+    func encrypt(_ plainText: Data, for recipientIdentifier: EncryptionSessionIdentifier) throws -> Data {
+        let key = hash(for: plainText, recepient: recipientIdentifier)
+
+        if let cachedObject = cache.value(for: key) {
+            zmLog.debug("Cache hit: \(key)")
+            return cachedObject
         }
         else {
-            zmLog.debug("Cache miss: \(cacheID)")
+            zmLog.debug("Cache miss: \(key)")
             let data = try backingEncryptor.encrypt(plainText, for: recipientIdentifier)
-            cache.setObject(data as NSData, forKey: cacheID, cost: data.count)
+            cache.set(value: data, for: key, cost: data.count)
             return data
         }
     }
